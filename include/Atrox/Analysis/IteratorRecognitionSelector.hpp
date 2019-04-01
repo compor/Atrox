d8faf2314785662232c48a7a2c56c0787165e99f
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-04-01 12:53:08 +0100

    Add builders for all types of dep graphs

--- include/Atrox/Analysis/IteratorRecognitionSelector.hpp
+++ include/Atrox/Analysis/IteratorRecognitionSelector.hpp
@@ -14,6 +14,7 @@ class Function;
 class BasicBlock;
 class LoopInfo;
 class Loop;
+class MemoryDependenceResults;
 } // namespace llvm
 
 namespace atrox {
@@ -21,12 +22,14 @@ namespace atrox {
 class IteratorRecognitionSelector {
   llvm::Function *CurFunc;
   llvm::LoopInfo *CurLI;
+  llvm::MemoryDependenceResults *CurMD;
   std::vector<llvm::BasicBlock *> Blocks;
 
   void calculate(llvm::Loop &L);
 
 public:
-  IteratorRecognitionSelector(llvm::Function &Func, llvm::LoopInfo &LI);
+  IteratorRecognitionSelector(llvm::Function &Func, llvm::LoopInfo &LI,
+                              llvm::MemoryDependenceResults *MD);
 
   const std::vector<llvm::BasicBlock *> &getBlocks(llvm::Loop &L) {
     calculate(L);
--- lib/Analysis/IteratorRecognitionSelector.cpp
+++ lib/Analysis/IteratorRecognitionSelector.cpp
@@ -6,20 +6,36 @@
 
 #include "Pedigree/Analysis/Creational/DDGraphBuilder.hpp"
 
+#include "Pedigree/Analysis/Creational/CDGraphBuilder.hpp"
+
+#include "Pedigree/Analysis/Creational/MDAMDGraphBuilder.hpp"
+
+#include "Pedigree/Support/Utils/InstIterator.hpp"
+
 #include "llvm/Analysis/LoopInfo.h"
 // using llvm::LoopInfo
 // using llvm::Loop
+
+#include "llvm/Analysis/MemoryDependenceAnalysis.h"
+// using llvm::MemoryDependenceResults
 
 #include "llvm/IR/Function.h"
 // using llvm::Function
 
 namespace atrox {
 
-IteratorRecognitionSelector::IteratorRecognitionSelector(llvm::Function &Func,
-                                                         llvm::LoopInfo &LI)
-    : CurFunc(&Func), CurLI(&LI) {
+IteratorRecognitionSelector::IteratorRecognitionSelector(
+    llvm::Function &Func, llvm::LoopInfo &LI, llvm::MemoryDependenceResults *MD)
+    : CurFunc(&Func), CurLI(&LI), CurMD(MD) {
   pedigree::DDGraphBuilder ddgBuilder{};
   auto ddgraph = ddgBuilder.setUnit(Func).ignoreConstantPHINodes(true).build();
+
+  pedigree::CDGraphBuilder cdgBuilder{};
+  auto cdgraph = cdgBuilder.setUnit(Func).build();
+
+  pedigree::MDAMDGraphBuilder mdgBuilder{};
+  auto mdgraph = mdgBuilder.setAnalysis(*CurMD).build(
+      pedigree::make_inst_begin(Func), pedigree::make_inst_end(Func));
 }
 
 void IteratorRecognitionSelector::calculate(llvm::Loop &L) {}
