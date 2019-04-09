a1a423b5d4e17e95473f9a9cdc61e05ccf48b352
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-04-09 19:44:37 +0100

    Add support for arg direction specification

---
+++ include/Atrox/Support/IR/ArgDirection.hpp
@@ -0,0 +1,39 @@
+//
+//
+//
+
+#pragma once
+
+#include "Atrox/Config.hpp"
+
+#include <cstdint>
+// using uint8_t
+
+namespace atrox {
+
+enum ArgDirection : uint8_t {
+  AD_Inbound = 1,
+  AD_Outbound = 2,
+  AD_Both = AD_Inbound | AD_Outbound,
+};
+
+inline bool isInbound(const ArgDirection AD) {
+  return (static_cast<int>(AD) & static_cast<int>(AD_Inbound));
+}
+
+inline bool isOutbound(const ArgDirection AD) {
+  return (static_cast<int>(AD) & static_cast<int>(AD_Outbound));
+}
+
+inline bool isOnlyInbound(const ArgDirection AD) {
+  return (static_cast<int>(AD) & static_cast<int>(AD_Inbound)) &&
+         !(static_cast<int>(AD) & static_cast<int>(AD_Outbound));
+}
+
+inline bool isOnlyOutbound(const ArgDirection AD) {
+  return (static_cast<int>(AD) & static_cast<int>(AD_Outbound)) &&
+         !(static_cast<int>(AD) & static_cast<int>(AD_Inbound));
+}
+
+} // namespace atrox
+
