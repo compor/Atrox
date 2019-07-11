ad85f6bbfb04f0c22b76d13bc3d6bc71da09652c
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-07-11 14:33:31 +0100

    Decompose array access by calculating indices

--- include/Atrox/Transforms/DecomposeMultiDimArrayRefs.hpp
+++ include/Atrox/Transforms/DecomposeMultiDimArrayRefs.hpp
@@ -6,11 +6,24 @@
 
 #include "Atrox/Config.hpp"
 
+#include "llvm/IR/Type.h"
+
+#include "llvm/ADT/SmallVector.h"
+
 namespace llvm {
 class GetElementPtrInst;
 } // namespace llvm
 
 namespace atrox {
+
+llvm::Type *GetDecomposedMultiDimArrayType(
+    const llvm::PointerType *PtrTy,
+    llvm::SmallVectorImpl<llvm::Type *> *NewTypes = nullptr);
+
+void GetDecomposedArraySizes(const llvm::PointerType *PtrTy,
+                             llvm::SmallVectorImpl<uint64_t> &Sizes);
+
+bool FlattenMultiDimArrayIndices(llvm::GetElementPtrInst *GEP);
 
 bool DecomposeMultiDimArrayRefs(llvm::GetElementPtrInst *GEP);
 
--- lib/Transforms/DecomposeMultiDimArrayRefs.cpp
+++ lib/Transforms/DecomposeMultiDimArrayRefs.cpp
@@ -19,6 +19,9 @@
 // using llvm::ArrayType
 // using llvm::PointerType
 
+#include "llvm/IR/IRBuilder.h"
+// using llvm::IRBuilder
+
 #include "llvm/Support/Debug.h"
 // using LLVM_DEBUG macro
 // using llvm::dbgs
@@ -27,41 +30,146 @@
 #include <vector>
 // using std::vector
 
+#include <iterator>
+// using std::back_inserter
+
+#include <algorithm>
+// using std::copy
+
 #define DEBUG_TYPE "atrox-decompose-array-refs"
 
 namespace atrox {
 
 llvm::Type *
 GetDecomposedMultiDimArrayType(const llvm::PointerType *PtrTy,
-                               llvm::SmallVectorImpl<llvm::Type *> &NewTypes) {
+                               llvm::SmallVectorImpl<llvm::Type *> *NewTypes) {
   if (!PtrTy) {
     return nullptr;
   }
 
-  unsigned levels = 0;
+  unsigned dim = 0;
 
   auto *elemTy = PtrTy->getElementType();
-  levels++;
+  dim++;
 
   llvm::Type *basicTy = nullptr;
   while (auto *arrayTy = llvm::dyn_cast<llvm::ArrayType>(elemTy)) {
     basicTy = elemTy = arrayTy->getElementType();
-    levels++;
+    dim++;
   }
 
   LLVM_DEBUG(llvm::dbgs() << "basic type: " << *basicTy << '\n'
-                          << "indirection levels: " << levels << '\n';);
+                          << "dimensions: " << dim << '\n';);
 
   auto *ptrTy = llvm::PointerType::getUnqual(basicTy);
-  NewTypes.push_back(ptrTy);
+  llvm::SmallVector<llvm::Type *, 8> newTypes{ptrTy};
 
-  for (auto i = 1u; i < levels; ++i) {
-    NewTypes.push_back(llvm::PointerType::getUnqual(NewTypes.back()));
+  for (auto i = 1u; i < dim; ++i) {
+    newTypes.push_back(llvm::PointerType::getUnqual(newTypes.back()));
   }
 
-  std::reverse(NewTypes.begin(), NewTypes.end());
+  std::reverse(newTypes.begin(), newTypes.end());
+
+  if (NewTypes) {
+    std::copy(newTypes.begin(), newTypes.end(), std::back_inserter(*NewTypes));
+  }
 
   return basicTy;
+}
+
+void GetDecomposedArraySizes(const llvm::PointerType *PtrTy,
+                             llvm::SmallVectorImpl<uint64_t> &Sizes) {
+  if (!PtrTy) {
+    return;
+  }
+
+  auto *elemTy = PtrTy->getElementType();
+
+  while (auto *arrayTy = llvm::dyn_cast<llvm::ArrayType>(elemTy)) {
+    elemTy = arrayTy->getElementType();
+    Sizes.push_back(arrayTy->getNumElements());
+  }
+
+  LLVM_DEBUG(llvm::dbgs() << "dimension sizes: ";
+             for (auto e
+                  : Sizes) { llvm::dbgs() << e << ' '; };
+             llvm::dbgs() << '\n';);
+
+  return;
+}
+
+bool FlattenMultiDimArrayIndices(llvm::GetElementPtrInst *GEP) {
+  bool changed = false;
+
+  if (!GEP) {
+    return changed;
+  }
+
+  llvm::SmallVector<uint64_t, 8> sizes;
+  GetDecomposedArraySizes(
+      llvm::dyn_cast<llvm::PointerType>(GEP->getPointerOperandType()), sizes);
+
+  if (sizes.size() <= 1) {
+    return changed;
+  }
+
+  llvm::SmallVector<uint64_t, 8> products;
+  products.resize(sizes.size());
+
+  products[0] = 1u;
+  std::reverse(sizes.begin(), sizes.end());
+
+  for (auto i = 1u; i < products.size(); ++i) {
+    products[i] = products[i - 1] * sizes[i - 1];
+  }
+
+  LLVM_DEBUG(llvm::dbgs() << "product sizes: ";
+             for (auto e
+                  : products) { llvm::dbgs() << e << ' '; };
+             llvm::dbgs() << '\n';);
+
+  llvm::SmallVector<llvm::Value *, 8> scaledIndices, summedIndices;
+  scaledIndices.resize(sizes.size());
+
+  llvm::SmallVector<llvm::Value *, 8> indices{std::next(GEP->idx_begin()),
+                                              GEP->idx_end()};
+  std::reverse(indices.begin(), indices.end());
+
+  llvm::IRBuilder<> builder{GEP};
+  changed = true;
+
+  for (auto i = 0; i < scaledIndices.size(); ++i) {
+    scaledIndices[i] = builder.CreateMul(
+        indices[i], builder.getInt64(products[i]), "scaled.idx");
+  }
+
+  for (auto i = 1; i < scaledIndices.size(); ++i) {
+    summedIndices.push_back(builder.CreateAdd(scaledIndices[i - 1],
+                                              scaledIndices[i], "summed.idx"));
+  }
+
+  llvm::SmallVector<llvm::Type *, 8> newTypes;
+
+  GetDecomposedMultiDimArrayType(
+      llvm::dyn_cast<llvm::PointerType>(GEP->getPointerOperandType()),
+      &newTypes);
+
+  auto *ptrCast = llvm::CastInst::CreatePointerCast(
+      GEP->getPointerOperand(), newTypes.back(), "ptrcast", GEP);
+
+  auto *elemTy =
+      llvm::dyn_cast<llvm::PointerType>(newTypes.back())->getElementType();
+  auto *newGEP = llvm::GetElementPtrInst::Create(
+      elemTy, ptrCast, summedIndices.back(), "decomposed.ptr", GEP);
+
+  if (auto *dbg = GEP->getMetadata("dbg")) {
+    newGEP->setMetadata("dbg", dbg);
+  }
+
+  GEP->replaceAllUsesWith(newGEP);
+  GEP->eraseFromParent();
+
+  return changed;
 }
 
 bool DecomposeMultiDimArrayRefs(llvm::GetElementPtrInst *GEP) {
@@ -80,7 +188,7 @@ bool DecomposeMultiDimArrayRefs(llvm::GetElementPtrInst *GEP) {
 
   GetDecomposedMultiDimArrayType(
       llvm::dyn_cast<llvm::PointerType>(GEP->getPointerOperandType()),
-      newTypes);
+      &newTypes);
 
   if (newTypes.size() != GEP->getNumIndices()) {
     // TODO check the relation with other composite type e.g. structs
